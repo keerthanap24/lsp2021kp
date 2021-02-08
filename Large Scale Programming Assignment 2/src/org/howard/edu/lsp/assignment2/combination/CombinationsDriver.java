5e548c7e1ebb2346f200abd0dbd3a6840e7d4cda
keerthanap24 <kpullela24@gmail.com>
2021-02-08 10:19:01 -0800

    updated driver class

--- Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/combination/CombinationsDriver.java
+++ Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/combination/CombinationsDriver.java
@@ -3,10 +3,11 @@ package org.howard.edu.lsp.assignment2.combination;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class CombinationsDriver extends Combinations {
+public class CombinationsDriver {
 	  public void main(String args[]) {
-		    Integer[] nums = {3,9,8,4,5,7,10};
-		    int sum = 15;
-		    findCombinations(new ArrayList<Integer>(Arrays.asList(nums)), sum);
+		  Combinations comb = new Combinations();
+		  Integer[] nums = {3,9,8,4,5,7,10};
+		  int sum = 15;
+		  comb.findCombinations(new ArrayList<Integer>(Arrays.asList(nums)), sum);
 	  }
 }
