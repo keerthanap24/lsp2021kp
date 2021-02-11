c7d8dbf67768f2c5fea84133be5c806df47e1ef5
keerthanap24 <kpullela24@gmail.com>
2021-02-11 13:08:12 -0800

    final combinations code

--- Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/combination/Combinations.java
+++ Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/combination/Combinations.java
@@ -4,30 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Combinations{
-  public void findCombinationsRecursive(ArrayList<Integer> nums, ArrayList<Integer> newNum, int sum){
-    int s = 0;
-    for (int x: newNum){
-      s += x;
-    }
-    if (s == sum){
-      System.out.println("sum(" + Arrays.toString(newNum.toArray()) + ")=" + sum);
-    }
-    if (s >= sum){
-      return;
-    }
-    for(int i = 0; i < nums.size(); i++) {
-      ArrayList<Integer> remainder = new ArrayList<Integer>();
-      int n = nums.get(i);
-      for (int j = i+1; j < nums.size(); j++){
-        remainder.add(nums.get(j));
-      }
-      ArrayList<Integer> newNum2 = new ArrayList<Integer>(newNum);
-      newNum2.add(n);
-      findCombinationsRecursive(remainder, newNum2,  sum);
-    }
-  }
-
-  public void findCombinations(ArrayList<Integer> nums, int sum) {
-    findCombinationsRecursive(nums, new ArrayList<Integer>(), sum);
+	/**
+	 * 
+	 * @param nums
+	 * @param newNum
+	 * @param sum
+	 */
+	public void findCombinationsRecursive(ArrayList<Integer> nums, ArrayList<Integer> newNum, int sum){
+	    int s = 0;
+	    for (int x: newNum){
+	      s += x;
+	    }
+	    if (s == sum){
+	      System.out.println("sum(" + Arrays.toString(newNum.toArray()) + ") = " + sum);
+	    }
+	    if (s >= sum){
+	      return;
+	    }
+	    for(int i = 0; i < nums.size(); i++) {
+	      ArrayList<Integer> remainder = new ArrayList<Integer>();
+	      int n = nums.get(i);
+	      for (int j = i + 1; j < nums.size(); j++){
+	        remainder.add(nums.get(j));
+	      }
+	      ArrayList<Integer> newNum2 = new ArrayList<Integer>(newNum);
+	      newNum2.add(n);
+	      findCombinationsRecursive(remainder, newNum2,  sum);
+	    }
+	  }
+	/**
+	 * 
+	 * @param nums
+	 * @param sum
+	 */
+	public void findCombinations(ArrayList<Integer> nums, int sum) {
+		findCombinationsRecursive(nums, new ArrayList<Integer>(), sum);
   }
 }
--- Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/driver/CombinationsDriver.java
+++ Large Scale Programming Assignment 2/src/org/howard/edu/lsp/assignment2/driver/CombinationsDriver.java
@@ -6,10 +6,29 @@ import java.util.Arrays;
 import org.howard.edu.lsp.assignment2.combination.Combinations;
 
 public class CombinationsDriver{
+	/**
+	 * 
+	 * @param args
+	 */
 	  public static void main(String args[]) {
 		  Combinations comb = new Combinations();
-		  Integer[] nums = {3,9,8,4,5,7,10};
+		  Integer[] nums = {3, 9, 8, 4, 5, 7, 10};
 		  int sum = 15;
 		  comb.findCombinations(new ArrayList<Integer>(Arrays.asList(nums)), sum);
+		  
+		  Combinations comb2 = new Combinations();
+		  Integer[] nums2 = {5, 10, 3, 17, 2};
+		  int sum2 = 27;
+		  comb2.findCombinations(new ArrayList<Integer>(Arrays.asList(nums2)), sum2);
+		  
+		  Combinations comb3 = new Combinations();
+		  Integer[] nums3 = {-3, 2, 5};
+		  int sum3 = 2;
+		  comb3.findCombinations(new ArrayList<Integer>(Arrays.asList(nums3)), sum3);
+		  
+		  Combinations comb4 = new Combinations();
+		  Integer[] nums4 = {0};
+		  int sum4 = 2;
+		  comb4.findCombinations(new ArrayList<Integer>(Arrays.asList(nums4)), sum4);
 	  }
 }
